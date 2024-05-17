a36537b83058b0bc57bc0a94bb03bd2a9b535726
Official-Owora <chukwuemeka.owora@gmail.com>
2024-05-17 14:05:48 +0100

    Added the Mapping profile and the Abstraction of the Program Service

---
+++ RegistrationPortal.Application/MapInitializer/ProfileMapping.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using RegistrationPortal.Domain.DTOs.Request.CreationDto;
+using RegistrationPortal.Domain.DTOs.Request.UpdateDto;
+using RegistrationPortal.Domain.DTOs.Response;
+using RegistrationPortal.Domain.Models;
+
+namespace RegistrationPortal.Application.MapInitializer
+{
+    public class ProfileMapping : Profile
+    {
+        public ProfileMapping()
+        {
+            CreateMap<ProgramRequestDto, Program>()
+                .ForMember(prog => prog.Questions,
+                config => config.MapFrom(progDto => progDto.questions))
+                .ForMember(prog => prog.CustomQuestions,
+                config => config.MapFrom(progDto => progDto.customQuestions));
+            CreateMap<Program, ProgramWithApplicationDto>();
+            CreateMap<QuestionRequestDto, Question>();
+            CreateMap<CustomQuestionsRequestDto, CustomQuestion>()
+                .ForMember(customQues => customQues.Choices,
+                config => config.MapFrom(customQuesDto => customQuesDto.choices));
+            CreateMap<Program, ProgramResponseDto>()
+                .ForMember(progDto => progDto.questions,
+                config => config.MapFrom(prog => prog.Questions))
+                .ForMember(progDto => progDto.customQuestions,
+                config => config.MapFrom(prog => prog.CustomQuestions));
+            CreateMap<Question, QuestionResponseDto>();
+            CreateMap<CustomQuestion, CustomQuestionResponseDto>()
+                .ForMember(cusQuesDto => cusQuesDto.choices,
+                config => config.MapFrom(cusQues => cusQues.Choices));
+            CreateMap<ChoiceRequestDto, Choice>();
+            CreateMap<Choice, ChoiceResponseDto>();
+            CreateMap<ProgramUpdateDto, Program>();
+            CreateMap<ChoiceUpdateDto, Choice>();
+            CreateMap<CustomQuestionUpdateDto, CustomQuestion>()
+                .ForMember(customQues => customQues.Choices,
+                config => config.MapFrom(customQuesDto => customQuesDto.choices));
+            CreateMap<QuestionUpdateDto, Question>();
+            CreateMap<CandidateAppRequestDto, CandidateApplication>()
+                .ForMember(app => app.Answers,
+                config => config.MapFrom(appDto => appDto.answers));
+            CreateMap<AnswerRequestDto, Answer>();
+            CreateMap<Answer, AnswerResponseDto>()
+                .ForMember(ansDto => ansDto.question,
+                config => config.MapFrom(ans => ans.Question))
+                .ForMember(ansDto => ansDto.customQuestion,
+                config => config.MapFrom(ans => ans.CustomQuestion));
+            CreateMap<CandidateApplication, CandidateAppResponseDto>()
+                .ForMember(appDto => appDto.answers,
+                config => config.MapFrom(app => app.Answers));
+        }
+    }
+}
---
+++ RegistrationPortal.Application/Services/Abstractions/IProgramService.cs
@@ -0,0 +1,21 @@
+﻿using RegistrationPortal.Common.Pagination;
+using RegistrationPortal.Domain.DTOs.Request.CreationDto;
+using RegistrationPortal.Domain.DTOs.Request.UpdateDto;
+using RegistrationPortal.Domain.DTOs.Response;
+using RegistrationPortal.Domain.DTOs.ResponseWrapper;
+using RegistrationPortal.Domain.Enums;
+
+namespace RegistrationPortal.Application.Services.Abstractions
+{
+    public interface IProgramService
+    {
+        Task<ResponseObject<ProgramResponseDto>> FindProgramByIdAsync(string programId);
+        Task<ResponseObject<PagedList<ProgramResponseDto>>> FindAllProgramAsync(PaginationParams paginationParams);
+        Task<ResponseObject<IEnumerable<CustomQuestionResponseDto>>> GetQuestionsByQuestionType(QuestionType questionType);
+        Task<ResponseObject<ProgramResponseDto>> CreateProgramAsync(ProgramRequestDto programRequestDto);
+        Task<ResponseObject<string>> DeleteQuestionByIdAsync(string programId);
+        Task<ResponseObject<ProgramResponseDto>> UpdateProgramAsync(ProgramUpdateDto programUpdateDto);
+        Task<ResponseObject<QuestionResponseDto>> UpdateQuestionAsync(QuestionUpdateDto questionUpdateDto);
+        Task<ResponseObject<CustomQuestionResponseDto>> UpdateCustomQuestionAsync(CustomQuestionUpdateDto customQuesUpdateDto);
+    }
+}
