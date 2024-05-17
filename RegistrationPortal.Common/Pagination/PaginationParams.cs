8153dc466116e1679aa6413e91d9aa3f42a6787b
Official-Owora <chukwuemeka.owora@gmail.com>
2024-05-17 12:29:28 +0100

    Added the RegistrationPortal.Common class library and added the Pagination classes for paging methods that returns a list.

---
+++ RegistrationPortal.Common/Pagination/PaginationParams.cs
@@ -0,0 +1,16 @@
+﻿namespace RegistrationPortal.Common.Pagination
+{
+    public class PaginationParams
+    {
+        private const int MaxPageSize = 50;
+        private int _pageNumber = 1;
+        public int PageNumber { get => _pageNumber; set => _pageNumber = (value < 1) ? _pageNumber : value; }
+        private int _pageSize = 10;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+    }
+}
